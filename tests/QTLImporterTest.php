de4a3715442f8cfd2d878a21b40f43d781520a05
Reynold <reynoldltan@gmail.com>
2021-02-22 11:44:06 -0600

    Echo class values

--- tests/QTLImporterTest.php
+++ tests/QTLImporterTest.php
@@ -112,7 +112,7 @@ class QTLImporterTest extends TripalTestCase {
     // Now we run the importer!
     $success = $importer->run();
     $this->assertNotFalse($success,
-      "The importer returned an error.");
+      "The importer returned an error....". $success);
 
     // Finally, check that the data is in the correct tables, etc.
     // -- check that the QTL features were created.
